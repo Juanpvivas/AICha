316bdf89a11661c41d66d4717c88c9aeaf64bdcc
juan.vivas <vivas.juanpablo@gmail.com>
2026-07-14 20:13:47 -0500

    style: update typography with custom AppTypography

--- app/src/main/java/com/juanpvivas/aichatjp/ui/theme/Theme.kt
+++ app/src/main/java/com/juanpvivas/aichatjp/ui/theme/Theme.kt
@@ -52,7 +52,7 @@ fun AIChaTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }
