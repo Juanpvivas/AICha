4f33a2022960b8ac7bac8f027dbb6a03438970dd
juan.vivas <vivas.juanpablo@gmail.com>
2026-07-16 09:09:48 -0500

    feat: integrate Hilt for dependency injection
    - Add Hilt, KSP and hilt-navigation-compose dependencies
    - Create AIChaApplication with @HiltAndroidApp
    - Add @AndroidEntryPoint to MainActivity
    - Register Application in AndroidManifest.xml
    
    Closes #5

--- app/src/main/java/com/juanpvivas/aichatjp/MainActivity.kt
+++ app/src/main/java/com/juanpvivas/aichatjp/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.juanpvivas.aichatjp.ui.chat.ChatRoute
 import com.juanpvivas.aichatjp.ui.theme.AiChatTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
